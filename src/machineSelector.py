0dab110463845faa8fdad85a809a2716b411db17
Alejandro Arroyo <alearroyov98@gmail.com>
2024-06-09 13:11:51 -0600

    adding the machineSelectro class

---
+++ src/machineSelector.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+class MachineSelector:
+
+    def __init__(self, filepath) -> None:
+        self.df_raw = pd.read_csv(filepath)
+        
+    def __dataPreparation(self) -> pd.DataFrame:
+
+        self.df_raw_share_columns = self.df_raw[self.df_raw.columns[:3]]
+        self.df_raw = self.df_raw.melt(self.df_raw.columns[:3], var_name='drumpress', value_name='value')
+        self.df_raw[['drumpress','variable']] = self.df_raw['drumpress'].str.split(pat='_',n=1,expand=True)
+
+        return self.df_raw
+
+    def Selection(self, machine:str):
+
+        df_prep = self.__dataPreparation()
+        machines = ['primary','secundary']
+
+        if machine in machines:
+            df_machine = df_prep[df_prep.drumpress == machine]
+            df_machine = df_machine.pivot(index='date_time',columns='variable',values='value')
+            df_machine = self.df_raw_share_columns.merge(df_machine,right_index=True, left_on='date_time')
+            df_machine.to_csv(f'./{machine}_drumpress_db.csv', index=False)
+        
+        else:
+            raise Exception('The inserted machine is not in the dataframe')
